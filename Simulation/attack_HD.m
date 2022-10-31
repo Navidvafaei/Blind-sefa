1b497d31c12ea90db302f9d500645acf9a1d8c67
Navid  Vafaei <30938963+Navidvafaei@users.noreply.github.com>
2022-10-31 23:42:45 +0330

    Update attack_HD.m

--- Simulation/attack_HD.m
+++ Simulation/attack_HD.m
@@ -16,16 +16,11 @@ cardA = length(A);
 cardB = length(B);
 pr_uni = 1/cardK;
 
-% % PART1: JOINT DISTRIBUTION ATTACK ON UNPROTECTED WITH ML DISTINGUISHER
-% 
-% % PART1A: Compute the theoretical distributions
 
 
-Distribution_Ineff = zeros(cardB, cardK);
-Distribution_Faulty = zeros(cardB, cardK);
-Distribution_Eff = zeros(cardB, cardK);
-Distribution_joint = zeros(cardA, cardB, cardK);
-Distribution_joint_HD = zeros(cardA, cardB, cardK);
+Distribution_SIFA = zeros(cardB, cardK);
+Distribution_SEFA1 = zeros(cardB, cardK);
+Distribution_SEFA2 = zeros(cardA, cardB, cardK);
 
 for k=K
     for a=0:255
@@ -35,34 +30,29 @@ for k=K
             b=bitand(a,faultvalue);
             hc=Hamming(bitxor(s_box(a+1),k));
             hf =Hamming(bitxor(s_box(b+1),k));
-            hz=Hamming(bitxor(s_box(b+1),s_box(a+1)));
+            hd=Hamming(bitxor(s_box(b+1),s_box(a+1)));
             % compute the joint distribution for every k
             if (b)==(a)
-                Distribution_Ineff(hf+1, k+1) = Distribution_Ineff(hf+1, k+1) + pr_uni;
+                Distribution_SIFA(hf+1, k+1) = Distribution_SIFA(hf+1, k+1) + pr_uni;
             else
-                Distribution_Faulty(hf+1, k+1) = Distribution_Faulty(hf+1, k+1) + pr_uni;
-                Distribution_Eff(hc+1, k+1) = Distribution_Eff(hc+1, k+1) + pr_uni;
-                Distribution_joint( hc+1, hf+1, k+1) = Distribution_joint( hc+1, hf+1, k+1) + (pr_uni);
-                Distribution_joint_HD( hc+1, hz+1, k+1) = Distribution_joint_HD( hc+1, hz+1, k+1) + (pr_uni);
+                Distribution_SEFA1(hf+1, k+1) = Distribution_SEFA1(hf+1, k+1) + pr_uni;
+                Distribution_SEFA2( hc+1, hd+1, k+1) = Distribution_SEFA2( hc+1, hd+1, k+1) + (pr_uni);
             end
         end
     end
 end
 
-PART1B: ML computation
+%PART1B: ML computation
 
-simulate the leakage using HW plus noise model
-we assume that we know the POIs involved in the leakage
+%simulate the leakage using HW plus noise model
+%we assume that we know the POIs involved in the leakage
 no_key=20;
-    Key_rank_ineff=zeros(no_traces,no_key);
-    Key_rank_Eff=zeros(no_traces,no_key);
-    Key_rank_Faulty=zeros(no_traces,no_key);
-    Key_rank_joint=zeros(no_traces,no_key);
-    Key_rank_joint_HD=zeros(no_traces,no_key);
-    Key_rank_ni=zeros(no_traces,no_key);
-    Key_rank_ne=zeros(no_traces,no_key);
-    Key_rank_nj=zeros(no_traces,no_key);
-    Key_rank_nj_HD=zeros(no_traces,no_key); 
+    Key_rank_SIFA_n=zeros(no_traces,no_key);%n denotes as a noisy circumstances
+    Key_rank_SEFA1_n=zeros(no_traces,no_key);
+    Key_rank_SEFA2_n=zeros(no_traces,no_key);
+    Key_rank_sifa=zeros(no_traces,no_key);
+    Key_rank_sefa1=zeros(no_traces,no_key);
+    Key_rank_sefa2=zeros(no_traces,no_key); 
 for no_k=1:no_key
     m = randi(range+1,no_traces,1)-1;
     Key = randi(256)-1;
@@ -78,7 +68,7 @@ for no_k=1:no_key
     L_HD=Hamming(HD) + normrnd(0,sigma,no_traces,1);
     %-------------------------------------------------------------------------
     
-    % estimation of sigma
+    % est_sifamation of sigma
     sigma_c =sigma; %std(L);
     sigma_f =sigma; %std(L_F);
     
@@ -86,130 +76,88 @@ for no_k=1:no_key
     
     % technique to compute ML distinguisher
     
-    score_Ineff = zeros(cardK,1);
-    score_Eff = zeros(cardK,1);
-    score_Faulty = zeros(cardK,1);
-    score_joint = zeros(cardK,1);
-    score_joint_HD = zeros(cardK,1);
+    score_SIFA = zeros(cardK,1);
+    score_SEFA1 = zeros(cardK,1);
+    score_SEFA2 = zeros(cardK,1);
     ni=0;
     ne=0;
-    esti=ones(256,1);
-    este=ones(256,1);
-    estj=ones(256,1);
-    estjHD=ones(256,1);
+    est_sifa=ones(256,1);%esti,ation of SIFA for noiseless secnario
+    est_sefa1=ones(256,1);
+    est_sefa2=ones(256,1);
     for i=1:no_traces
         for k=K
-            summation_Ineff = 0;
-            summation_Eff = 0;
-            summation_Faulty = 0;
-            summation_joint = 0;
-            summation_joint_HD = 0;
+            summation_SIFA = 0;
+            summation_SEFA1 = 0;
+            summation_SEFA2 = 0;
             if c(i)==c_faulty(i)
                 ni=ni+1;
-                    esti(k+1)=log(Distribution_Ineff(Hamming(c(i))+1,k+1))+esti(k+1);
-                for h = A
-                    term1i = (1/sigma_c)*sqrt(2*pi) * exp(-0.5 * ((L(i) - h)/sigma_c)^2 );
-                    term2i = Distribution_Ineff(h+1,k+1);
+                    est_sifa(k+1)=log(Distribution_SIFA(Hamming(c(i))+1,k+1))+est_sifa(k+1);
+                for hc = A
+                    term1i = (1/sigma_c)*sqrt(2*pi) * exp(-0.5 * ((L(i) - hc)/sigma_c)^2 );
+                    term2i = Distribution_SIFA(hc+1,k+1);
                     full_termi = term1i * term2i;
-                    summation_Ineff = summation_Ineff + full_termi;
+                    summation_SIFA = summation_SIFA + full_termi;
                 end
                 if ni>256
-                    score_Ineff(k+1) = score_Ineff(k+1) + log(summation_Ineff);
+                    score_SIFA(k+1) = score_SIFA(k+1) + log(summation_SIFA);
                 else
-                    score_Ineff(k+1) =  log(summation_Ineff);
+                    score_SIFA(k+1) =  log(summation_SIFA);
                 end
                 
             else
                 ne=ne+1;
-                    este(k+1)=log(Distribution_Faulty(Hamming(c_faulty(i))+1,k+1))+este(k+1);
-                    estj(k+1)=log(Distribution_joint(Hamming(c(i))+1, Hamming(c_faulty(i))+1,k+1))+estj(k+1);
-                    estjHD(k+1)=log(Distribution_joint_HD(Hamming(c(i))+1, Hamming(HD(i))+1,k+1))+estjHD(k+1);
-                for h = A
-                    term1e = (1/sigma_c)*sqrt(2*pi) * exp(-0.5 * ((L(i) - h)/sigma_c)^2 );
-                    term2e = Distribution_Eff(h+1,k+1);
-                    full_terme = term1e * term2e;
-                    summation_Eff = summation_Eff + full_terme;
-                    for h_f = B
-                        if h==0
-                            term1f = (1/sigma_f)*sqrt(2*pi) * exp(-0.5 * ((L_F(i) - h_f)/sigma_f)^2 );
-                            term2f = Distribution_Faulty(h_f+1,k+1);
+                    est_sefa1(k+1)=log(Distribution_SEFA1(Hamming(c_faulty(i))+1,k+1))+est_sefa1(k+1);
+                    est_sefa2(k+1)=log(Distribution_SEFA2(Hamming(c(i))+1, Hamming(HD(i))+1,k+1))+est_sefa2(k+1);
+                for hc = A
+                    for hf = B 
+                        if hc==0
+                            term1f = (1/sigma_f)*sqrt(2*pi) * exp(-0.5 * ((L_F(i) - hf)/sigma_f)^2 );
+                            term2f = Distribution_SEFA1(hf+1,k+1);
                             full_termf = term1f * term2f;
-                            summation_Faulty = summation_Faulty + full_termf;
+                            summation_SEFA1 = summation_SEFA1 + full_termf;
                         end
-                        term1j = (1/sigma_c)*sqrt(2*pi) * exp(-0.5 * ((L(i) - h)/sigma_c)^2 ) * (1/sigma_f)*sqrt(2*pi) * exp(-0.5 * ((L_F(i) - h_f)/sigma_f)^2 );
-                        term2j = Distribution_joint(h+1,h_f+1,k+1);
-                        full_termj = term1j * term2j;
-                        summation_joint = summation_joint + full_termj;
-                        term1jHD = (1/sigma_c)*sqrt(2*pi) * exp(-0.5 * ((L(i) - h)/sigma_c)^2 ) * (1/sigma_f)*sqrt(2*pi) * exp(-0.5 * ((L_HD(i) - h_f)/sigma_f)^2 );
-                        term2jHD = Distribution_joint_HD(h+1,h_f+1,k+1);
+                        term1jHD = (1/sigma_c)*sqrt(2*pi) * exp(-0.5 * ((L(i) - hc)/sigma_c)^2 ) * (1/sigma_f)*sqrt(2*pi) * exp(-0.5 * ((L_HD(i) - hf)/sigma_f)^2 );
+                        term2jHD = Distribution_SEFA2(hc+1,hf+1,k+1);
                         full_termjHD = term1jHD * term2jHD;
-                        summation_joint_HD = summation_joint_HD + full_termjHD;
+                        summation_SEFA2 = summation_SEFA2 + full_termjHD;
                         
                     end
                 end
                 if ne>256
-                    score_Eff(k+1) = score_Eff(k+1) +log( summation_Eff);
-                    score_Faulty(k+1) = score_Faulty(k+1) + log(summation_Faulty);
-                    score_joint(k+1) = score_joint(k+1) +log( summation_joint);
-                    score_joint_HD(k+1) = score_joint_HD(k+1) +log( summation_joint_HD);
+                    score_SEFA1(k+1) = score_SEFA1(k+1) + log(summation_SEFA1);
+                    score_SEFA2(k+1) = score_SEFA2(k+1) +log( summation_SEFA2);
                 else
-                    score_Eff(k+1) =log(summation_Eff);
-                    score_Faulty(k+1) =log(summation_Faulty);
-                    score_joint(k+1) =log(summation_joint);
-                    score_joint_HD(k+1) =log(summation_joint_HD);
+                    score_SEFA1(k+1) =log(summation_SEFA1);
+                    score_SEFA2(k+1) =log(summation_SEFA2);
                 end
                 
             end
         end
-        Key_rank_ineff(i,no_k)=sum(score_Ineff(:)>= score_Ineff(Key+1));
-        Key_rank_Eff(i,no_k)=sum(score_Eff(:)>= score_Eff(Key+1));
-        Key_rank_Faulty(i,no_k)=sum(score_Faulty(:)>= score_Faulty(Key+1));
-        Key_rank_joint(i,no_k)=sum(score_joint(:)>= score_joint(Key+1));
-        Key_rank_joint_HD(i,no_k)=sum(score_joint_HD(:)>= score_joint_HD(Key+1));
+        Key_rank_SIFA_n(i,no_k)=sum(score_SIFA(:)>= score_SIFA(Key+1));
+        Key_rank_SEFA1_n(i,no_k)=sum(score_SEFA1(:)>= score_SEFA1(Key+1));
+        Key_rank_SEFA2_n(i,no_k)=sum(score_SEFA2(:)>= score_SEFA2(Key+1));
 %%%########################################################################        
-        Key_rank_ni(i,no_k)=sum(esti(:)>= esti(Key+1));
-        Key_rank_ne(i,no_k)=sum(este(:)>= este(Key+1));
-        Key_rank_nj(i,no_k)=sum(estj(:)>= estj(Key+1));
-        Key_rank_nj_HD(i,no_k)=sum(estjHD(:)>= estjHD(Key+1));
+        Key_rank_sifa(i,no_k)=sum(est_sifa(:)>= est_sifa(Key+1));
+        Key_rank_sefa1(i,no_k)=sum(est_sefa1(:)>= est_sefa1(Key+1));
+        Key_rank_sefa2(i,no_k)=sum(est_sefa2(:)>= est_sefa2(Key+1));
     end
     ni=ni/256;
     ne=ne/256;
 
 end
 hold on
-plot(mean( Key_rank_ineff(1:no_traces,1:no_key),2),'red','LineWidth',1)
-plot(mean (Key_rank_Eff(1:no_traces,:),2),'LineWidth',1)
-plot( mean (Key_rank_Faulty(1:no_traces,1:no_key),2),'green','LineWidth',1)
-% plot( mean(Key_rank_joint(1:no_traces,:),2),'blue','LineWidth',1)
-plot( mean(Key_rank_joint_HD(1:no_traces,1:no_key),2),'blue','LineWidth',1)
-% 
-% % 
+plot(mean( Key_rank_SIFA_n(1:no_traces,1:no_key),2),'red','LineWidth',1)
+plot( mean (Key_rank_SEFA1_n(1:no_traces,1:no_key),2),'green','LineWidth',1)
+plot( mean(Key_rank_SEFA2_n(1:no_traces,1:no_key),2),'blue','LineWidth',1)
+
 hold off
 % legend('sifa','sefFault','sefa')
-% % savings=zeros(2,no_traces);
-% % savings(1,:)=mean( Key_rank_ineff(1:no_traces,:),2);
-% % savings(2,:)=mean (Key_rank_Eff(1:no_traces,:),2);
-% % savings(3,:)=mean (Key_rank_Faulty(1:no_traces,:),2);
-% % savings(4,:)=mean(Key_rank_joint(1:no_traces,:),2);
-% % savings(4,:)=mean(Key_rank_joint_HD(1:no_traces,:),2);
-% % 
-% % csvwrite('file415.csv',savings)
-% % 
+
 hold on
-plot(mean( Key_rank_ni(1:no_traces,:),2),'red','LineWidth',1)
-% plot(mean (Key_rank_Eff(1:no_traces,:),2),'LineWidth',1)
-plot( mean (Key_rank_ne(1:no_traces,:),2),'green','LineWidth',1)
-% % plot( mean(Key_rank_nj(1:no_traces,:),2),'blue','LineWidth',1)
-plot( mean(Key_rank_nj_HD(1:no_traces,:),2),'blue','LineWidth',1)
-% 
-hold off
-legend('sifa','sefa')
-% savings=zeros(2,no_traces);
-% savings(1,:)=mean( Key_rank_ni(1:no_traces,:),2);
-% % savings(2,:)=mean (Key_rank_Eff(1:no_traces,:),2);
-% % savings(3,:)=mean (Key_rank_ne(1:no_traces,:),2);
-% savings(2,:)=mean(Key_rank_nj_HD(1:no_traces,:),2);
-% csvwrite('file4ni7.csv',savings)
+plot(mean( Key_rank_sifa(1:no_traces,:),2),'red','LineWidth',1)
+plot( mean (Key_rank_sefa1(1:no_traces,:),2),'green','LineWidth',1)
+plot( mean(Key_rank_sefa2(1:no_traces,:),2),'blue','LineWidth',1)
+
 
 
 
